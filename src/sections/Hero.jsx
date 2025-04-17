306df76528c3453b88bd4dc3e2d66f4e543d3f39
Jacob Habashi <jhabashi@dillard.edu>
2025-04-17 01:09:19 -0500

    update

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -89,7 +89,7 @@ const Hero = () => {
     <section className='min-h-screen w-full flex flex-col relative border-2 border-blue-500'>
       <div className='w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3'>
         <p className='sm:text-3xl text-2xl font-medium text-white text-center font-generalsans'>
-          Hi, I am Maryan <span className='waving-hand'>👋</span>
+          Hi, I am Maryann <span className='waving-hand'>👋</span>
         </p>
         <p className='hero_tag text-gray_gradient'>Building Products</p>
       </div>
